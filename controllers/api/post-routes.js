567fc188d20833875a1bbd3334233b2b7ae1acf8
jcaswell <jakecaswell88@gmail.com>
2021-11-28 15:58:24 -0500

    added comments and upvotes for single posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -86,13 +86,16 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
+// make sure the session exists first
+if (req.session) {
     // custom static method created in models/Post.js
-    Post.upvote(req.body, {Vote})
-        .then(dbPostData => res.json(dbPostData))
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(upvotedPostData => res.json(upvotedPostData))
         .catch(err => {
             console.log(err);
-            res.status(400).json(err);
+            res.status(500).json(err);
         })
+}
 });
 
 router.put('/:id', (req, res) => {
